7d73c295dcad0eadf00591f40a42b091651e5025
Andra-Teodora Cîrciumaru <teoidk@gmail.com>
2024-09-25 13:38:06 +0300

    Delete src directory

--- src/main/java/com/example/demo/exceptions/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exceptions;
-
-public class BookNotFoundException extends RuntimeException{
-
-    public BookNotFoundException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/example/demo/exceptions/MovieNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exceptions;
-
-public class MovieNotFoundException extends RuntimeException{
-
-    public MovieNotFoundException(String message){
-       super(message);
-    }
-}
